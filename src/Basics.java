46bb887b57e0a011f1429df07dd4f046bd8498d6
Anirudha0313 <anirudhsuryawanshi03@gmail.com>
2026-01-17 19:52:49 +0530

    Prime number question solved

--- src/Basics.java
+++ src/Basics.java
@@ -2,14 +2,34 @@ import java.util.Scanner;
 
 public class Basics {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+      /*  Scanner input = new Scanner(System.in);
         int a =  input.nextInt();
         int b =  input.nextInt();
         int c = a * b ;
         int d = a+b;
         System.out.println("Multiplication= " + c);
-        System.out.println("Addition= " + d);
+        System.out.println("Addition= " + d); 
+        
+       */
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
 
+        if (n <= 1) {
+            System.out.println("Not Prime");
+        } else {
+            int i;
+            for (i = 2; i * i <= n; i++) {
+                if (n % i == 0) {
+                    System.out.println("Not Prime");
+                    break;
+                }
+            }
+
+            if (i * i > n) {
+                System.out.println("Prime");
+            }
+        }
 
     }
 }
