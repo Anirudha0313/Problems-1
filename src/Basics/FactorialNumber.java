7a86730556b5915c6e066617bdf3a6c3f94f93ad
Anirudha0313 <anirudhsuryawanshi03@gmail.com>
2026-01-26 23:37:50 +0530

    Add FactorialNumber

---
+++ src/Basics/FactorialNumber.java
@@ -0,0 +1,23 @@
+package Basics;
+import java.util.Scanner;
+
+public class FactorialNumber {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        int fact = 1;  // factorial starts from 1
+
+        for (int i = 1; i <= num; i++) {
+            fact = fact * i;
+        }
+
+        System.out.println("Factorial = " + fact);
+
+        sc.close();
+    }
+}
