be9704a3cb468e54b1d6c584930ce0c1928823f5
Anirudha0313 <anirudhsuryawanshi03@gmail.com>
2026-01-30 13:11:18 +0530

    PalindromeString

---
+++ src/Basics/PalindromeString.java
@@ -0,0 +1,36 @@
+package Basics;
+import java.util.Scanner;
+
+public class PalindromeString {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        boolean isPalindrome = true;
+
+        int start = 0;
+        int end = str.length() - 1;
+
+        while (start < end) {
+
+            if (str.charAt(start) != str.charAt(end)) {
+                isPalindrome = false;
+                break;
+            }
+
+            start++;
+            end--;
+        }
+
+        if (isPalindrome)
+            System.out.println("Palindrome String");
+        else
+            System.out.println("Not a Palindrome String");
+
+        sc.close();
+    }
+}
