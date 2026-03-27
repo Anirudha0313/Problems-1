f30bedade4f2d945c640938ba574be0924bec46b
Anirudha0313 <anirudhsuryawanshi03@gmail.com>
2026-03-27 22:42:54 +0530

    Update

--- src/OOP/Main.java
+++ src/OOP/Main.java
@@ -1,61 +1,14 @@
 package OOP;
-import java.util.Scanner;
-
-class Student {
-
-    String name;
-    int rollNo;
-    int marks;
-
-    // Constructor
-    Student(String name, int rollNo, int marks) {
-        this.name = name;
-        this.rollNo = rollNo;
-        this.marks = marks;
-    }
-
-    // Method to calculate grade
-    char calculateGrade() {
-
-        if (marks >= 90)
-            return 'A';
-        else if (marks >= 75)
-            return 'B';
-        else if (marks >= 50)
-            return 'C';
-        else
-            return 'F';
-    }
-
-    // Method to display details
-    void display() {
-
-        System.out.println("Name: " + name);
-        System.out.println("Roll No: " + rollNo);
-        System.out.println("Marks: " + marks);
-        System.out.println("Grade: " + calculateGrade());
-    }
-}
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+        BankAccount acc1 = new BankAccount("Ravi", 101, 5000);
 
-        System.out.print("Enter name: ");
-        String name = sc.nextLine();
-
-        System.out.print("Enter roll number: ");
-        int roll = sc.nextInt();
-
-        System.out.print("Enter marks: ");
-        int marks = sc.nextInt();
-
-        Student s1 = new Student(name, roll, marks);
-
-        s1.display();
-
-        sc.close();
+        acc1.displayBalance();
+        acc1.deposit(2000);
+        acc1.withdraw(1500);
+        acc1.displayBalance();
     }
 }
